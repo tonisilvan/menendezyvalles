94f9ebdad73b957d2e9db3aedb99435796da38e3
tonisilvan <toni.silvan.online@gmail.com>
2024-08-31 23:59:51 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ function plusSlides(n, containerId) {
 
 function loadImages(containerId, folderName, totalImages) {
     var container = document.getElementById(containerId);
-    for (var i = 1; i <= totalImages; i++) {
+    for (var i = 0; i <= totalImages; i++) {
         var slideDiv = document.createElement('div');
         slideDiv.className = 'carousel-slide';
         var img = document.createElement('img');
@@ -133,8 +133,8 @@ function loadImages(containerId, folderName, totalImages) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    loadImages('carousel-container-menedez', 'menendez', 29); // Carga 29 imágenes para Casa Menéndez
-    loadImages('carousel-container-valles', 'valles', 57); // Carga 57 imágenes para Casa Valles
+    loadImages('carousel-container-menedez', 'menendez', 42); // Carga 29 imágenes para Casa Menéndez
+    loadImages('carousel-container-valles', 'valles', 51); // Carga 57 imágenes para Casa Valles
 });
 
 document.getElementById('mobile-menu').addEventListener('click', function () {
